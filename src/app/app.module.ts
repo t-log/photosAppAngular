45804f21559953da049ddc75ccb8c4a1b9ea9999
akhil <akhil.paul@ymail.com>
2022-11-29 15:10:52 +0530

    Adds fetch from url and view feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViewPhotosComponent } from './view-photos/view-photos.component';
+import { HttpClientModule } from '@angular/common/http';
+
+const myRoute:Routes=[{path:"",component:ViewPhotosComponent}]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewPhotosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot(myRoute),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
